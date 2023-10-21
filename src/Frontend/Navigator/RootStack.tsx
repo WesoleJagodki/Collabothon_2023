97bfa3894809d8a68e85784e395b1638d4c26f8a
Daman2137 <109245610+Daman2137@users.noreply.github.com>
2023-10-21 22:01:57 +0200

    [FIX] Start from welcomescreen

--- src/Frontend/Navigator/RootStack.tsx
+++ src/Frontend/Navigator/RootStack.tsx
@@ -8,7 +8,6 @@ import { RegistrationFinishedScreen } from '../Screen/RegistrationFinished/Regis
 import { SignupLoginScreen } from '../Screen/SignupLogin_Screen/SignupLoginScreen';
 import { Tutorial1, Tutorial2, Tutorial3 } from '../Screen/Tutorial/TutorialScreen';
 import { WelcomeScreen } from '../Screen/Welcome/WelcomeScreen';
-import {SignupLoginScreen} from "../Screen/SignupLogin_Screen/SignupLoginScreen";
 import {ChooseInterestsScreen} from "../Screen/ChooseInterests/ChooseInterestsScreen";
 
 const Stack = createStackNavigator();
@@ -16,7 +15,7 @@ const Stack = createStackNavigator();
 export function RootStack() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Root" component={ChooseInterestsScreen} options={{ title: 'Splash' }} />
+      <Stack.Screen name="Root" component={WelcomeScreen} options={{ title: 'Splash' }} />
       <Stack.Screen name="TutorialScreen1" component={Tutorial1} options={{ title: 'Tutorial' }} />
       <Stack.Screen name="TutorialScreen2" component={Tutorial2} options={{ title: 'Tutorial' }} />
       <Stack.Screen name="TutorialScreen3" component={Tutorial3} options={{ title: 'Tutorial' }} />
