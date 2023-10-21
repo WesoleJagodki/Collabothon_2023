1426b0e40295806310adf91191e18846b2a43529
Preacherek <sarna2000piotr@gmail.com>
2023-10-21 23:09:43 +0200

    [FEAT] Finished connect app to DB

--- src/Frontend/Screen/Login/LoginScreen.tsx
+++ src/Frontend/Screen/Login/LoginScreen.tsx
@@ -1,9 +1,76 @@
-import { Button, Text, Box, Center, HStack, Input, InputField, Image } from '@gluestack-ui/themed';
+import {
+  Button,
+  Text,
+  Box,
+  Center,
+  HStack,
+  Input,
+  InputField,
+  Image,
+  InputSlot,
+  InputIcon,
+  EyeIcon, EyeOffIcon
+} from '@gluestack-ui/themed';
+import axios from 'axios';
 
 import { loginScreen } from './LoginScreenStyle';
 import { commonStyle } from '../CommonStyle';
+import React from "react";
 
 export function LoginScreen({ navigation }: any) {
+  const [emailString, setEmailString] = React.useState('');
+  const [passwordString, setPasswordString] = React.useState('');
+  const [DBID, setDBID] = React.useState('');
+  const [DBPasswordString, setDBPasswordString] = React.useState('');
+  const [showPassword, setShowPassword] = React.useState(false);
+  const [connectionFlag, setConnectionFlag] = React.useState(0);
+
+  const handleState = () => {
+    setShowPassword((showState) => {
+      return !showState;
+    });
+  };
+
+  const baseUrl_Password = 'http://localhost:3000/users_passwords';
+  const baseUrl_Email = 'http://localhost:3000/users_emails';
+
+  const GetPassword = () => {
+    axios
+        .get(`${baseUrl_Password}`, {
+          headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET',
+          },
+        })
+        .then((response) => {
+          console.log('Get DB data: ' + JSON.parse(JSON.stringify(response.data[0])).password);
+          if (JSON.parse(JSON.stringify(response.data[0])).password === passwordString) {
+            navigation.navigate('AccountDetailsScreen');
+          }
+        });
+  };
+
+  const GetEmail = () => {
+    axios
+        .get(`${baseUrl_Email}`, {
+          headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET',
+          },
+        })
+        .then((response) => {
+          console.log('Get DB data: ' +  JSON.parse(JSON.stringify(response.data[0])).user_id);
+          setDBID(JSON.parse(JSON.stringify(response.data[0])).user_id);
+          GetPassword();
+        });
+  };
+
+  const LogIn = async () => {
+      if (emailString !== '') {
+        GetEmail();
+      }
+  }
+
   return (
     <Box style={commonStyle.appScreen}>
       <Box style={loginScreen.content_box}>
@@ -13,18 +80,22 @@ export function LoginScreen({ navigation }: any) {
         </Center>
         <Text style={loginScreen.text}>Email</Text>
         <Input style={loginScreen.input}>
-          <InputField style={loginScreen.input_text} placeholder="Enter email" />
+          <InputField style={loginScreen.input_text} placeholder="Enter email" onChangeText={setEmailString}/>
         </Input>
         <Text style={loginScreen.text}>Password</Text>
         <Input style={loginScreen.input}>
-          <InputField style={loginScreen.input_text} placeholder="Enter password" />
+          <InputField style={loginScreen.input_text} placeholder="Enter password" type={showPassword ? 'text' : 'password'}
+                      onChangeText={setPasswordString} />
+          <InputSlot pr="$3" onPress={handleState}>
+              <InputIcon as={showPassword ? EyeIcon : EyeOffIcon} color="#CAD1DB" />
+          </InputSlot>
         </Input>
         <Text style={loginScreen.text2} onPress={() => navigation.navigate('AccountDetailsScreen')}>
           Forgot Password?
         </Text>
         <Button
           style={loginScreen.button}
-          onPress={() => navigation.navigate('AccountDetailsScreen')}>
+          onPress={() => LogIn()}>
           <Text style={loginScreen.button_text}>LOG IN</Text>
         </Button>
         <Center>
