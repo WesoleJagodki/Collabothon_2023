86d6c725437b64f5dabd37fcf1e1396f36b96da0
andrew2a1 <adam.brz@onet.eu>
2023-10-22 01:44:10 +0200

    Welcome screen is first

--- src/Frontend/Navigator/RootStack.tsx
+++ src/Frontend/Navigator/RootStack.tsx
@@ -16,7 +16,7 @@ const Stack = createStackNavigator();
 export function RootStack() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Root" component={RootTab} options={{ title: 'Splash' }} />
+      <Stack.Screen name="Root" component={WelcomeScreen} options={{ title: 'Splash' }} />
       <Stack.Screen name="TutorialScreen1" component={Tutorial1} options={{ title: 'Tutorial' }} />
       <Stack.Screen name="TutorialScreen2" component={Tutorial2} options={{ title: 'Tutorial' }} />
       <Stack.Screen name="TutorialScreen3" component={Tutorial3} options={{ title: 'Tutorial' }} />
