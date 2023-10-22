08d92d047401327e91f53b581e4c7421464c6291
andrew2a1 <adam.brz@onet.eu>
2023-10-22 02:13:15 +0200

    Final diagrams

--- src/Frontend/Screen/Dashboard.tsx
+++ src/Frontend/Screen/Dashboard.tsx
@@ -3,6 +3,7 @@ import { commonStyle } from './CommonStyle';
 import { MonthlySpendingsGraph } from '../Components/MonthlySpendingsGraph';
 
 export function Dashboard({ navigation }: any) {
+  // TODO: Add real data
   const thisMonth = [
     { dayOfMonth: 1, spending: Math.random() * 100 },
     { dayOfMonth: 2, spending: Math.random() * 100 },
@@ -15,6 +16,7 @@ export function Dashboard({ navigation }: any) {
     { dayOfMonth: 2, spending: Math.random() * 100 },
     { dayOfMonth: 3, spending: Math.random() * 100 },
     { dayOfMonth: 4, spending: Math.random() * 100 },
+    { dayOfMonth: 5, spending: Math.random() * 100 },
   ];
   return (
     <Box style={commonStyle.appScreen}>
